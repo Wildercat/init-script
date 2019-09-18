556c45ba1792fdba2d1d33845893d3f1a50e0f3f
Wildercat <39809284+Wildercat@users.noreply.github.com>
2019-09-18 17:51:05 -0400

    Update init-script.sh

--- init-script.sh
+++ init-script.sh
@@ -6,11 +6,14 @@
 # Gather constant vars
 CURRENTDIR=${PWD##*/}
 GITHUBUSER=$(git config github.userName)
+
+# ------------ ATTN: CONFIGURATION REQUIRED ------------
 # You will need to create a file with either your password or an api key and point to it here
 APIKEY=$(cat api_key)
 # Storing your password in plaintext isn't great security-wise, you can make an api key here: https://github.com/settings/tokens
 # just make sure to check "repo" to give that key the right permissions
 
+# ------------ ATTN: CONFIGURATION REQUIRED ------------
 # Replace my username with your own
 USER=wildercat
 
@@ -26,10 +29,13 @@ echo "Here we go..."
 curl -u ${USER}:${APIKEY} https://api.github.com/user/repos -d "{\"name\": \"${REPONAME:-${CURRENTDIR}}\", \"description\": \"${DESCRIPTION}\", \"private\": false, \"has_issues\": true, \"has_downloads\": true, \"has_wiki\": false}"
 
 # Create new directory in your development or sites directory with same name as repo
+# ------------ ATTN: CONFIGURATION REQUIRED ------------
 # Mine is ~dev/, you'll need to change it to wherever your dir is
 mkdir ~/dev/${REPONAME}
 
 # git init in new dir
+# ------------ ATTN: CONFIGURATION REQUIRED ------------
+# need to change your folder here as well
 cd ~/dev/${REPONAME}
 git init
 # Add new repo as remote
